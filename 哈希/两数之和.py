7cee5f5af4247ff2bc2fb89066b4494a90cfede1
panyu0666-wq <panyu0666@gmail.com>
2026-07-24 09:12:06 +0800

    20260724

--- 哈希/两数之和.py
+++ 哈希/两数之和.py
@@ -1,4 +1,10 @@
 # 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target  的那两个整数，并返回它们的数组下标。
 # 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
 # 你可以按任意顺序返回答案。
-
+def twoSum(nums,target):
+    records = dict()
+    for index,value in enumerate(records):
+        if target - value in records:    
+            return [records[target-value],index]
+        records[value] = index
+    return []
